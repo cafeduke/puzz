ef502043474c5e181af59402cefc5a84bbc0320d
Raghunandan.Seshadri <raghubs81@gmail.com>
2018-10-27 08:04:00 +0530

    Update

--- make_large_num.py
+++ make_large_num.py
@@ -26,10 +26,10 @@ def cmp (a, b):
 def cmp_large(a, b):
     '''
     Comparator to compare two integers based on most significant number (left most) first.
-    A number with fewer digits is considered to be greater than the one with fewer digits. Eg: 7 > 78, 789 < 800, 9 > 800 
+    A number with fewer digits is considered to be greater than the one with fewer digits. Eg: 7 < 78 because 778 < 787
     
     Parameters
-    ----------
+    ----------d
     a : integer
         First integer to be compared
 
@@ -44,19 +44,7 @@ def cmp_large(a, b):
         Zero            : a == b
     '''
     a, b = str(a), str(b)
-    na, nb = len(a), len(b)
-
-    # If the number of digits are same, comparision is same as string comparision (MSB to LSB)
-    if na == nb:
-        return cmp (a, b)
-
-    # Slice the strings to min of the two lengths, and compare them. In essence, we are not using the extra portion of the larger string for the comparison.
-    ln = min (na, nb)
-    val = cmp(a[:ln], b[:ln])
-
-    # A 'val' of 0  indicates larger string starts with same digits as smaller (like '78' and ''78965'). Return smaller string to be higher.
-    # If a = '78' and b = '78965' then nb - na == 5 - 2 == 3. A positive value indicates a > b (as expected)
-    return nb - na if val == 0 else val
+    return cmp (a+b, b+a)
 
 def main ():
     l = [78, 789, 9, 998, 989, 0]
